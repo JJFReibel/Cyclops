41b91b4bf40df5ffe90dfac53840c057542c58fa
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 15:15:06 -0700

    uploaded cyclops.py

---
+++ cyclops.py
@@ -0,0 +1,29 @@
+# By Jean-Jacques F. Reibel
+# Cyclops from X-Men
+# I will not be held responsible for:
+# any shenanigans
+# ಠ_ಠ
+# ¯¯\_(ツ)_/¯¯
+import os
+ 
+
+os.system("printf '\e[0;34;1;1m\n        Cyclops\n'")
+os.system("printf '\e[0;34;1;1m          __\n'")
+os.system("printf '\e[0;33;1;1m         ('")
+os.system("printf '\e[0;31;1;5mಠಠ'")
+os.system("printf '\e[0;33;1;1m)'")
+os.system("printf '\e[0;31;1;5m-------------------------------\n'")
+os.system("printf '\e[0;35;1;1m          \_|\n'")
+os.system("printf '\e[0;34;1;1m         _| |_\n'")
+os.system("printf '\e[0;34;1;1m        /|   |\\'")
+os.system("printf '\e[0;33;1;1m  @\n'")
+os.system("printf '\e[0;34;1;1m       / |   | \\/\n'")
+os.system("printf '\e[0;34;1;1m      /  |___|  \n'")
+os.system("printf '\e[0;33;1;1m     /`  |___|   \n'")
+os.system("printf '\e[0;33;1;1m         \___/\n'")
+os.system("printf '\e[0;34;1;1m         /   \\\n'")
+os.system("printf '\e[0;34;1;1m        /     \\\n'")
+os.system("printf '\e[0;34;1;1m       |       |\n'")
+os.system("printf '\e[0;34;1;1m       |       |\n'")
+os.system("printf '\e[0;33;1;1m       |__     |__\n'")
+os.system("printf '\e[0;39;1;1m\n\n'")
